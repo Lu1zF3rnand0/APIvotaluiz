2094df1f2de467f5c00392a254fe13832a506dbe
Luiz <luizfernando.silva@outlook.com>
2022-06-19 08:56:39 -0300

    Initial commit

---
+++ src/main/java/br/edu/infnet/apivotaluiz/ApIvotaluizApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apivotaluiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIvotaluizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIvotaluizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/apivotaluiz/ApIvotaluizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apivotaluiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIvotaluizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
